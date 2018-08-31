bec3de940d4f20075aa835d06a46f57245d5f88a
Tanner Lee <tannerlee@mymail.mines.edu>
2018-08-31 12:07:35 -0600

    Completed section 1.4 problem 4

--- HW2_S1.4_P4.jl
+++ HW2_S1.4_P4.jl
@@ -5,3 +5,23 @@ Homework 2
 Section 1.4
 Problem 4
 =#
+
+# Create the first Matrix
+A = [1 3 -4 ;
+     3 2 1]
+
+# Create the second matrix
+B = [1 ; 2 ; 1]
+
+# Multiply the A and B
+matrixProduct = A*B
+
+# Print the results
+println()
+println("Original Matrix, A:")
+println(A)
+println("Original Matrix, B")
+println(B)
+println("Product Matrix, A*B:")
+println(matrixProduct)
+println()
