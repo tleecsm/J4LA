f20282df8df123e881d98a74052ded73f154cd21
Tanner Lee <tannerlee@mymail.mines.edu>
2018-08-31 11:17:53 -0600

    Added logic to section 1.3 problem 13.  Testing incomplete.

--- HW2_S1.3_P13.jl
+++ HW2_S1.3_P13.jl
@@ -14,8 +14,66 @@ A = [1 -4 2 3 ;
 # Store the number of columns
 columns = 4
 
-# Solve the matrix
+# Print the original Matrix
+println("Original Matrix:")
+println(A)
+println()
+
+# Row reduce the Matrix
+A_rref = RowEchelon.rref(A)
+
+# Print the RREF'd matrix
+println("Row Reduced Echelon Form:")
+println(A_rref)
+println()
+
+# Determine whether or not matrix was consistent
+index = 0
+leadingEntryLocated = false
+consistent = true
 for value in A
-    print(value)
-    print(" | | ")
+    # Determine first if we are in the 0 column of a Row
+    if index % columns == 0
+        # If here then:
+        # We are in the 0 columns
+        # Reset Leading Entry Locator
+        leadingEntryLocated = false
+    end
+    # Now we can save time by skipping the rest of the Row
+    # If a leading entry has already been found
+    if leadingEntryLocated == false
+        # If here then:
+        # Leading entry hasnt been found yet
+        # Evaluate the current position
+        # Determine if its a leading entry
+        if value != 0
+            # If here then:
+            # This row has a leading entry somewhere
+            if index % columns == columns - 1
+                # If here then:
+                # This entry is in the last column
+                # Matrix is not consistent
+                consistent = false
+            else
+                # Else:
+                # The matrix hasnt been proven inconsistent
+                leadingEntryLocated = true
+            end
+        end
+    end
+        index = index+1
+end
+
+if consistent == true
+    # If here then:
+    # Matrix is consistent
+    println("This matrix is consistent.")
+    print("Hence the vector b is a linear combination of the ")
+    print(" vectors made by the columns of the original matrix.")
+else
+    # Else:
+    # Matrix is inconsistent
+    println("This matrix is inconsistent.")
+    print("Hence the vector b is NOT a linear combination of the ")
+    print(" vectors made by the columns of the original matrix.")
 end
