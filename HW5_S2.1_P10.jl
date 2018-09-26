01c982f145eba732c73703c70a2b3e255c462ef8
Tanner Lee <tannerlee@mymail.mines.edu>
2018-09-25 23:43:46 -0600

    Completed HW5 S2.1 P10

--- HW5_S2.1_P10.jl
+++ HW5_S2.1_P10.jl
@@ -5,3 +5,21 @@ Homework 5
 Section 2.1
 Problem 10
 =#
+
+# Initialize Matrices
+A = [3 -6 ; -1 2]
+B = [-1 1 ; 3 4]
+C = [-3 -5 ; 2 1]
+
+print("AB = ")
+println(A*B)
+
+print("AC = ")
+println(A*C)
+
+println("Determine whether B == C:")
+if (B == C)
+	println("True, B == C")
+else
+	println("False, B != C")
+end
