f34c47a17a79080a5cef8b294b6b893873e9983d
Tanner Lee <tannerlee@mymail.mines.edu>
2018-09-25 23:46:37 -0600

    Completed HW5 S2.2 P2

--- HW5_S2.2_P2.jl
+++ HW5_S2.2_P2.jl
@@ -3,5 +3,14 @@ import RowEchelon
 #=
 Homework 5
 Section 2.2
-Problem 1
+Problem 2
 =#
+
+# Initialize the Matrix
+A = [3 2 ; 8 5]
+
+# Calculate the Inverse
+invA = inv(A)
+
+println("The Inverse of A is:")
+println(invA)
