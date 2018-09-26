aeda2e2ce380eceaf0788541506d42367d2d5af1
Tanner Lee <tannerlee@mymail.mines.edu>
2018-09-25 23:36:28 -0600

    Completed HW5 S2.1 P1

--- HW5_S2.1_P1.jl
+++ HW5_S2.1_P1.jl
@@ -5,3 +5,32 @@ Homework 5
 Section 2.1
 Problem 1
 =#
+
+# Initialize all Matrices
+A = [2 0 -1 ; 4 -5 2]
+B = [7 -5 1 ; 1 -4 -3]
+C = [1 2 ;-2 1]
+D = [3 5 ; -1 4]
+E = [-5 ; 3]
+
+println("-2A:")
+println(-2*A)
+
+println("B-2A:")
+println(B-(2*A))
+
+# After this point, products may not exist
+# Use try/catch to find nonexistent products.
+println("AC:")
+try
+	println(A*C)
+catch
+	println("AC cannot be computed.")
+end
+
+println("CD:")
+try
+	println(C*D)
+catch
+	println("CD cannot be computed.")
+end
