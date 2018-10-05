ad74e07b3bf6c65bd199ef2325779afb6c1d6a0f
Tanner Lee <tannerlee@mymail.mines.edu>
2018-10-04 18:04:19 -0600

    Completed S2.3 P44.

--- HW6_S2.3_44.jl
+++ HW6_S2.3_44.jl
@@ -5,3 +5,19 @@ Homework 6
 Section 2.3
 Problems 44
 =#
+
+# Create matrix to be inverted
+A = [1 1/2 1/3 1/4 1/5 ;
+     1/2 1/3 1/4 1/5 1/6 ;
+     1/3 1/4 1/5 1/6 1/7 ;
+     1/4 1/5 1/6 1/7 1/8 ;
+     1/5 1/6 1/7 1/8 1/9]
+
+invA = inv(A)
+
+# print the last column
+println("The last column of the inverse is:")
+for i=1:5
+    println(invA[i,5])
+end
+println("Additional decimal places are a result of floating point error.")
