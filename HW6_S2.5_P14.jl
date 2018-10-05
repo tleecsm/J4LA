5491140a8cce88e753fd47cf9caaa1fc6521eb78
Tanner Lee <tannerlee@mymail.mines.edu>
2018-10-04 18:30:34 -0600

    Completed S2.5 H14

--- HW6_S2.5_P14.jl
+++ HW6_S2.5_P14.jl
@@ -1,7 +1,16 @@
 import RowEchelon
+import LinearAlgebra
 
 #=
 Homework 6
 Section 2.5
 Problems 14
 =#
+
+# Create the matrix
+A = [ 1 3 1 5 ; 5 20 6 31 ; -2 -1 -1 -4 ; -1 7 1 7]
+LU = LinearAlgebra.lu(A)
+print("L = \t")
+println(LU.L)
+print("U = \t")
+println(LU.U)
