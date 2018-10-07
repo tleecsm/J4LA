be33c39a77a1c556edc8a49874064233cba3d748
Tanner Lee <tannerlee@mymail.mines.edu>
2018-10-07 16:15:07 -0600

    Finished HW7, P29.

--- HW7_S3.2_P29.jl
+++ HW7_S3.2_P29.jl
@@ -6,3 +6,17 @@ Homework 7
 Section 3.2
 Problems 29
 =#
+
+# Create B
+B = [1 0 1 ; 1 1 2 ; 1 2 1]
+# Create B^5
+B5 = B^5
+# Get detB5
+detB5 = LinearAlgebra.det(B5)
+
+println("Original Matrix, B:")
+println(B)
+println("B^5:")
+println(B5)
+println("Determinate of B^5")
+println(detB5)
