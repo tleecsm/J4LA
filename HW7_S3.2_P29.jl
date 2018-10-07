d28cb9459058c4275d6804dd5caef4d9a49b75d5
Tanner Lee <tannerlee@mymail.mines.edu>
2018-10-07 15:52:49 -0600

    prepped files for HW7.

--- HW7_S3.2_P29.jl
+++ HW7_S3.2_P29.jl
@@ -2,27 +2,7 @@ import RowEchelon
 import LinearAlgebra
 
 #=
-Homework 6
-Section 2.5
-Problems 17
+Homework 7
+Section 3.2
+Problems 29
 =#
-
-# Create the matrix
-A = [2 -6 4 ; -4 8 0 ; 0 -4 6]
-LU = LinearAlgebra.lu(A)
-print("L = \t")
-println(LU.L)
-print("U = \t")
-println(LU.U)
-
-# Create inverses of LU
-invL = inv(LU.L)
-invU = inv(LU.U)
-print("invL = \t")
-println(invL)
-print("invU = \t")
-println(invU)
-
-# Solve for invA
-print("invA = \t")
-println(invU*invL)
