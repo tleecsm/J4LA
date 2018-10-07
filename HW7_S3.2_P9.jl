861c2ad4e0703051b02a903854fddbd65ab5e0b9
Tanner Lee <tannerlee@mymail.mines.edu>
2018-10-07 16:05:35 -0600

    Finished HW7, P9.

--- HW7_S3.2_P9.jl
+++ HW7_S3.2_P9.jl
@@ -6,3 +6,15 @@ Homework 7
 Section 3.2
 Problems 9
 =#
+
+A = [1 -1 -3 0 ;
+     0 1 5 4 ;
+     -1 2 8 5;
+     3 -1 -2 3]
+
+detA = LinearAlgebra.det(A)
+
+println("Original Matrix:")
+println(A)
+println("Determinate:")
+println(detA)
