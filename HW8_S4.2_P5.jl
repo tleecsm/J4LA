960f66da20a5e84498503b429ec6836c71be7539
Tanner Lee <tannerlee@mymail.mines.edu>
2018-10-15 13:10:16 -0600

    Problem 5 completed.

--- HW8_S4.2_P5.jl
+++ HW8_S4.2_P5.jl
@@ -2,7 +2,17 @@ import RowEchelon
 import LinearAlgebra
 
 #=
-Homework 7
-Section 3.3
-Problems 19
+Homework 8
+Section 4.2
+Problems 5
 =#
+
+A = [1 -4 0 2 0 ;
+     0 0 1 -5 0 ;
+     0 0 0 0 2]
+
+nulA = LinearAlgebra.nullspace(A)
+println("Original Matrix, A:")
+println(A)
+println("NulA:")
+println(nulA)
