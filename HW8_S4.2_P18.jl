1221279806506817e2f0e4c398b99b5637bdf5ed
Tanner Lee <tannerlee@mymail.mines.edu>
2018-10-15 08:00:53 -0600

    Prepared files for HW8 submission.

---
+++ HW8_S4.2_P18.jl
@@ -0,0 +1,8 @@
+import RowEchelon
+import LinearAlgebra
+
+#=
+Homework 8
+Section 4.2
+Problems 18
+=#
---
+++ HW8_S4.2_P5.jl
@@ -0,0 +1,8 @@
+import RowEchelon
+import LinearAlgebra
+
+#=
+Homework 7
+Section 3.3
+Problems 19
+=#
