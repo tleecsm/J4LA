0ff51fa1771e9b5fc0b8a684a65cb36cebb8eefb
Tanner Lee <tannerlee@mymail.mines.edu>
2018-10-15 13:16:23 -0600

    Hw8 completed.

--- HW8_S4.2_P18.jl
+++ HW8_S4.2_P18.jl
@@ -6,3 +6,16 @@ Homework 8
 Section 4.2
 Problems 18
 =#
+
+A = [5 -2 3 ;
+     -1 0 -1 ;
+     0 -2 -2 ;
+     -5 7 2]
+
+sizeA = size(A)
+println("Original Matrix, A:")
+println(A)
+print("NulA is a subspace of R")
+println(sizeA[2])
+print("ColA is a subspace of R")
+println(sizeA[1])
